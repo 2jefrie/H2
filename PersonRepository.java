2f840f9e848fa22e368b589b6869ff6853b15745
2jefrie <161617181+2jefrie@users.noreply.github.com>
2024-08-14 11:19:41 -0600

    Add files via upload

---
+++ PersonRepository.java
@@ -0,0 +1,7 @@
+package com.example.h2demo;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface PersonRepository extends CrudRepository<Person, Long> {
+    Person save(Person person);
+}
